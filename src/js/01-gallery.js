8f58d7a7b30b5be1c7b2ed48d0885d2e354aa299
YaroslavBoliachevets <boliachevets.ya@gmail.com>
2022-05-11 23:30:03 +0300

    установ библиотеку и скопировал галерею

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
 // Add imports above this line
+// Описан в документации
+import SimpleLightbox from "simplelightbox";
+// Дополнительный импорт стилей
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 import { galleryItems } from './gallery-items';
+
 // Change code below this line
 
 console.log(galleryItems);
+
+const divImgEl = document.querySelector('.gallery');
+
+const makeGalleryItemMurkup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `
+    <a class="gallery__link" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+	`;
+  })
+  .join('');
+
+divImgEl.insertAdjacentHTML('beforeend', makeGalleryItemMurkup);
+
+let gallery = new SimpleLightbox('.gallery a', { captionsData:'alt', captionDelay: 250 });
